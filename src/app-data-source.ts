8b6a8876f75eda07002fa90bb7209da81d6652e3
Redwan <redwanfarisa@gmail.com>
2023-06-25 05:36:56 -0400

    fix: change to relative path

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -2,12 +2,12 @@ import { DataSource } from "typeorm"
 ​
 export const myDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     username: "root",
     password: "root",
-    database: "qaf_bot",
+    database: "qaf",
     entities: ["src/entities/*{.js,.ts}"],
     logging: true,  
-    synchronize:true
+ 
 })
--- src/bot/helpers.ts
+++ src/bot/helpers.ts
@@ -1,26 +1,32 @@
-import axios from "axios"
-import * as fs from 'fs';
-import * as crypto from 'crypto';
-import * as path from 'path'
- 
-export const downloadMedia = async (fileUrls:string[]) => {
-    let paths:string[] = [];
-    for(let i =0;i<fileUrls.length;i++){
-        const fileExtension = path.extname(fileUrls[i]);
- 
-        const randomString = crypto.randomBytes(6).toString('hex');
-        const uniqueFilename = `${Date.now()}_${randomString}${fileExtension}`;
-        const filePath = `/home/redwan/personal/coding/project/qaf-bot/public/${uniqueFilename}`;
-        const writeStream = fs.createWriteStream(filePath);
-        const response = await axios.get(fileUrls[i], { responseType: "stream" });
-        response.data.pipe(writeStream);
-        
-        writeStream.on('finish', () => {
-            paths.push(uniqueFilename);
-          });
-          writeStream.on('error', (error) => {
-            console.error('Error saving photo:', error);
-          });
+import axios from "axios";
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+
+export const downloadMedia = async (fileUrls: string[]) => {
+  const publicDir = path.join(process.cwd(),  'public');
+  const paths: string[] = [];
+
+  for (let i = 0; i < fileUrls.length; i++) {
+    try {
+      const fileExtension = path.extname(fileUrls[i]);
+      const randomString = crypto.randomBytes(6).toString('hex');
+      const uniqueFilename = `${Date.now()}_${randomString}${fileExtension}`;
+      const filePath = path.join(publicDir, uniqueFilename);
+      const writeStream = fs.createWriteStream(filePath);
+      const response = await axios.get(fileUrls[i], { responseType: "stream" });
+      response.data.pipe(writeStream);
+
+      await new Promise<void>((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+      });
+
+      paths.push(uniqueFilename);
+    } catch (error) {
+      console.error('Error saving photo:', error);
     }
-    return paths;
   }
+
+  return paths;
+};
