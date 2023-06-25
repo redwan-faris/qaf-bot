957c2fc06e74ad1ec8d77cbe040d4683bb9f99e4
Redwan <redwanfarisa@gmail.com>
2023-06-25 06:00:30 -0400

    refactor: change bot to class"

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,27 @@
-    import express from 'express';
- 
-    import dotenv from 'dotenv';
-    import { myDataSource } from './app-data-source';
-    import routes from "./routes";
-import { bot } from './bot/bot';
+import express from 'express';
+import dotenv from 'dotenv';
+import { myDataSource } from './app-data-source';
+import routes from "./routes";
+import { Bot } from './bot/bot';
 
-    dotenv.config();
-    const app = express();
-    app.use(express.json());
+dotenv.config();
+const app = express();
+app.use(express.json());
 
-    myDataSource
-        .initialize()
-        .then(() => {
-            console.log("Data Source has been initialized!")
-            bot.launch()
-        })
-        .catch((err) => {
-            console.error("Error during Data Source initialization:", err)
-        })
-    app.use('/', routes);
+const bot = new Bot();
+bot.start();
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
 
+app.use('/', routes);
 
-
-    const port = process.env.PORT ;
-    app.listen( port || 3000, () => {
-    console.log(`Express server started on port ${port}`);
-    });
+const port = process.env.PORT;
+app.listen(port || 3000, () => {
+  console.log(`Express server started on port ${port}`);
+});
--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,199 +1,219 @@
 import { Telegraf, Context } from "telegraf";
-import {  message } from "telegraf/filters";
-import dotenv from "dotenv"; 
+import { message } from "telegraf/filters";
+import dotenv from "dotenv";
 import { EventInterface } from "../types/event.type";
 import { downloadMedia } from "./helpers";
-import { saveEvent, saveMedia ,getBotMessages} from "./api";
+import { saveEvent, saveMedia, getBotMessages } from "./api";
 import { Event } from "../entities/Event";
- 
-
 
 dotenv.config();
 const token = process.env.BOT_TOKEN;
-let step = "";
-let event:EventInterface = {
-  description:"",
-  reporter:"",
-  address:"",
-  media:[] as string[]
-};
-if (!token) {
-  console.error("BOT_TOKEN is not defined in the environment variables");
-  process.exit(1);
+
+export class Bot {
+  private bot: Telegraf<Context>;
+  private step: string;
+  private event: EventInterface;
+
+  constructor() {
+    if (!token) {
+      console.error("BOT_TOKEN is not defined in the environment variables");
+      process.exit(1);
+    }
+
+    this.bot = new Telegraf(token);
+    this.step = "";
+    this.event = {
+      description: "",
+      reporter: "",
+      address: "",
+      media: [] as string[],
+    };
+
+    this.setupCommands();
+    this.setupActions();
+    this.setupMessageHandlers();
+    this.setupMiddleware();
+  }
+
+  private setupCommands(): void {
+    this.bot.command("start", (ctx: Context) => {
+      ctx.reply("مرحبا بك في بوت انا مراسل");
+    });
+
+    this.bot.command("option1", (ctx) => {
+      ctx.telegram.sendMessage(ctx.chat.id, "كيف تريد ارسال المعلومات؟", {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "انا مراسل",
+                callback_data: "reporter",
+              },
+              {
+                text: "انا مدون",
+                callback_data: "location",
+              },
+            ],
+          ],
+        },
+      });
+    });
+  }
+
+  private setupActions(): void {
+    this.bot.action("reporter", (ctx) => {
+      ctx.deleteMessage();
+      ctx.replyWithHTML("من فضلك اكتب اسم المراسل:", {
+        reply_markup: {
+          force_reply: true,
+        },
+      });
+
+      this.step = "location";
+    });
+
+    this.bot.action("location", (ctx) => {
+      ctx.deleteMessage();
+      ctx.replyWithHTML("من فضلك أدخل الموقع:", {
+        reply_markup: {
+          force_reply: true,
+        },
+      });
+      this.step = "event";
+    });
+
+    this.bot.action("mediaAccept", (ctx) => {
+      ctx.deleteMessage();
+      ctx.replyWithHTML("من فضلك أدخل الصورة او الفديو", {
+        reply_markup: {
+          force_reply: true,
+        },
+      });
+      this.step = "media";
+    });
+
+    this.bot.action("mediaDecline", async (ctx) => {
+      ctx.deleteMessage();
+      ctx.replyWithHTML("شكرا لمشاركتك المعلومات");
+      this.step = "finish";
+      let paths = await downloadMedia(this.event.media);
+      const newEvent: Event = await saveEvent(this.event);
+      await saveMedia(paths, newEvent);
+      console.log(paths);
+    });
+  }
+
+  private setupMessageHandlers(): void {
+    this.bot.on(message("text"), (ctx) => {
+      if (this.step === "location") {
+        const reporterName = ctx.message.text;
+        if (reporterName) {
+          this.event.reporter = reporterName;
+        }
+        ctx.replyWithHTML("من فضلك أدخل الموقع:", {
+          reply_markup: {
+            force_reply: true,
+          },
+        });
+        this.step = "event";
+      } else if (this.step === "event") {
+        const location = ctx.message.text;
+        this.event.address = location;
+        ctx.replyWithHTML("من فضلك أدخل الحدث:", {
+          reply_markup: {
+            force_reply: true,
+          },
+        });
+        this.step = "media";
+      } else if (this.step == "media") {
+        const description = ctx.message.text;
+        this.event.description = description;
+        ctx.telegram.sendMessage(ctx.chat.id, "هل تريد مشاركة صور او فديوهات تخص الحدث؟", {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "نعم",
+                  callback_data: "mediaAccept",
+                },
+                {
+                  text: "لا",
+                  callback_data: "mediaDecline",
+                },
+              ],
+            ],
+          },
+        });
+      }
+    });
+
+    this.bot.on(message("photo"), async (ctx) => {
+      if (this.step === "media") {
+        const photo = ctx.message.photo[ctx.message.photo.length - 1];
+
+        const fileId = photo.file_id;
+        const file = await ctx.telegram.getFile(fileId);
+        const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+        this.event.media.push(fileUrl);
+        ctx.telegram.sendMessage(ctx.chat.id, "هل توجد صور  او فديوهات اخرى؟", {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "نعم",
+                  callback_data: "mediaAccept",
+                },
+                {
+                  text: "لا",
+                  callback_data: "mediaDecline",
+                },
+              ],
+            ],
+          },
+        });
+      }
+    });
+
+    this.bot.on(message("video"), async (ctx) => {
+      if (this.step === "media") {
+        const video = ctx.message.video;
+
+        const fileId = video.file_id;
+        const file = await ctx.telegram.getFile(fileId);
+        const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+
+        this.event.media.push(fileUrl);
+        ctx.telegram.sendMessage(ctx.chat.id, "هل توجد صور  او فديوهات اخرى؟", {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "نعم",
+                  callback_data: "mediaAccept",
+                },
+                {
+                  text: "لا",
+                  callback_data: "mediaDecline",
+                },
+              ],
+            ],
+          },
+        });
+      }
+    });
+  }
+
+  private setupMiddleware(): void {
+    this.bot.use((ctx: Context, next: () => Promise<void>) => {
+      console.log("Received update:", ctx.update);
+      return next();
+    });
+  }
+
+  public async start(): Promise<void> {
+    await this.bot.launch();
+    console.log("Bot is running...");
+  }
 }
 
-export const bot = new Telegraf(token);
  
-
-bot.command("start", (ctx: Context) => {
-  ctx.reply("مرحبا بك في بوت انا مراسل");
-});
-
-bot.command("option1", (ctx) => {
-  ctx.telegram.sendMessage(ctx.chat.id, "كيف تريد ارسال المعلومات؟", {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "انا مراسل",
-            callback_data: "reporter",
-          },
-          {
-            text: "انا مدون",
-            callback_data: "location",
-          },
-        ],
-      ],
-    },
-  });
-});
-
-bot.action("reporter", (ctx) => {
-  ctx.deleteMessage();
-  ctx.replyWithHTML("من فضلك اكتب اسم المراسل:", {
-    reply_markup: {
-      force_reply: true,
-    },
-  });
-
-  step = "location";
-});
-
-bot.action("location", (ctx) => {
-  ctx.deleteMessage();
-  ctx.replyWithHTML("من فضلك أدخل الموقع:", {
-    reply_markup: {
-      force_reply: true,
-    },
-  });
-  step = "event";
-});
-
-bot.on(message("text"), (ctx) => {
-  if (step === "location") {
-    const reporterName = ctx.message.text;
-    if(reporterName){
-      event.reporter = reporterName;
-    }
-    ctx.replyWithHTML("من فضلك أدخل الموقع:", {
-      reply_markup: {
-        force_reply: true,
-      },
-    });
-    step = "event";
-  } else if (step === "event") {
-    const location = ctx.message.text;
-    event.address = location;
-    ctx.replyWithHTML("من فضلك أدخل الحدث:", {
-      reply_markup: {
-        force_reply: true,
-      },
-    });
-    step = "media";
-  } else if (step == "media") {
-    const description = ctx.message.text;
-    event.description = description;
-    ctx.telegram.sendMessage(ctx.chat.id, "هل تريد مشاركة صور او فديوهات تخص الحدث؟", {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "نعم",
-              callback_data: "mediaAccept",
-            },
-            {
-              text: "لا",
-              callback_data: "mediaDecline",
-            },
-          ],
-        ],
-      },
-    });
-  }
-});
-
-bot.action("mediaAccept", (ctx) => {
-  ctx.deleteMessage();
-  ctx.replyWithHTML("من فضلك أدخل الصورة او الفديو", {
-    reply_markup: {
-      force_reply: true,
-    },
-  });
-  step = "media";
-});
-
-
- 
-bot.action("mediaDecline", async (ctx) => {
-  console.log(event)  
-  ctx.deleteMessage();
-  ctx.replyWithHTML("شكرا لمشاركتك المعلومات");
-  step = "finish";
-  let paths = await downloadMedia(event.media);
-  const newEvent:Event = await saveEvent(event);
-  await saveMedia(paths,newEvent)
-  console.log(paths)
-});
-
-bot.on(message("photo"),async  (ctx) => {
-  if (step === "media") {
-    const photo = ctx.message.photo[ctx.message.photo.length -1];
-  
-    const fileId = photo.file_id;
-    const file = await ctx.telegram.getFile(fileId);
-    const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
-    event.media.push(fileUrl);
-    ctx.telegram.sendMessage(ctx.chat.id, "هل توجد صور  او فديوهات اخرى؟", {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "نعم",
-              callback_data: "mediaAccept",
-            },
-            {
-              text: "لا",
-              callback_data: "mediaDecline",
-            },
-          ],
-        ],
-      },
-    });
-  
-  }
-});
-
-bot.on(message("video"),async  (ctx) => {
-  if (step === "media") {
-    const photo = ctx.message.video ;
-  
-    const fileId = photo.file_id;
-    const file = await ctx.telegram.getFile(fileId);
-    const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
- 
-    event.media.push(fileUrl);
-    ctx.telegram.sendMessage(ctx.chat.id, "هل توجد صور  او فديوهات اخرى؟", {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "نعم",
-              callback_data: "mediaAccept",
-            },
-            {
-              text: "لا",
-              callback_data: "mediaDecline",
-            },
-          ],
-        ],
-      },
-    });
-  
-  }
-});
-
-
-bot.use((ctx: Context, next: () => Promise<void>) => {
-  console.log("Received update:", ctx.update);
-  return next();
-});
